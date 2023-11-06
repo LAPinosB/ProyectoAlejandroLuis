f707cb920e1c363d8a1362733e98094e3829c65f
Alejandro Verdejo <averdejo128@hotmail.com>
2023-11-06 19:45:23 +0100

    Commit 0

---
+++ ProyectoAlejandroLuis/src/proyectoalejandroluis/AbrirDoc.java
@@ -0,0 +1,17 @@
+
+package proyectoalejandroluis;
+
+import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
+public class AbrirDoc {
+    
+    Document doc;
+    
+    
+}
