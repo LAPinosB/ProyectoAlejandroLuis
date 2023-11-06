ab0eed87c5db5bdbb57260402dd7e477ee77a817
Luis Pinos <149178337+LAPinosB@users.noreply.github.com>
2023-11-06 20:13:12 +0100

    version 1.3
    Configuracion del Main

--- ProyectoAlejandroLuis/src/proyectoalejandroluis/ProyectoAlejandroLuis.java
+++ ProyectoAlejandroLuis/src/proyectoalejandroluis/ProyectoAlejandroLuis.java
@@ -1,9 +1,21 @@
 package proyectoalejandroluis;
-import java.util.Scanner;
+import java.io.File;
+import org.w3c.dom.Document;
+
+/**
+ *
+ * @author Alejandro , luis
+ */
+
 public class ProyectoAlejandroLuis{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
+        Document doc;
+        File f = new File("books.xml");
+        AbrirDoc abrirDoc = new AbrirDoc();
+        LeerAccesoDom leerAccesoDom = new LeerAccesoDom();
+        doc = abrirDoc.abrirDoc(f);
+        leerAccesoDom.mostrarLibros(doc);
+        
     }
     
 }
