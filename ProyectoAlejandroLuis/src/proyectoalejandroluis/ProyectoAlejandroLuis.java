ace7322e359ba885777c90ad4676872108ab944e
Luis Pinos <149178337+LAPinosB@users.noreply.github.com>
2023-11-06 21:22:27 +0100

    Version 1.5
    Guardar en un nuevo Archivo Proyecto

--- ProyectoAlejandroLuis/src/proyectoalejandroluis/ProyectoAlejandroLuis.java
+++ ProyectoAlejandroLuis/src/proyectoalejandroluis/ProyectoAlejandroLuis.java
@@ -15,17 +15,33 @@ public class ProyectoAlejandroLuis{
         try {
             //Para que funcionen las ñ
             System.setOut(new PrintStream(System.out, true, "UTF-8"));
+            // Creacion del doc que usaremos para los metodos.
             Document doc;
             File f = new File("books.xml");
+            
+            //Instaciamos la clase Abrir Doc
             AbrirDoc abrirDoc = new AbrirDoc();
+            //Instaciamos la clase LeearAccesoDom
             LeerAccesoDom leerAccesoDom = new LeerAccesoDom();
+            //Instaciamos la clase Eliminar Nodo
             EliminarNodo eliminarNodo = new EliminarNodo();
+            //Instaciamos la clase Insertar Nodo
             InsertarDatosDOM insertarDatos = new InsertarDatosDOM();
+            //Instanciamos la clase Guardar Archivo
+            GuardarDomNuevoArchivo guardarDomNuevoArchivo = new GuardarDomNuevoArchivo();
+            
+            
             doc = abrirDoc.abrirDoc(f);
-            //leerAccesoDom.mostrarLibros(doc);
+            
+            //leerAccesoDom.mostrarLibros(doc); // Primera Prueba de que lee el acceso dom correctamente.
             doc = eliminarNodo.deleteNode("Midnight Rain", doc);
             doc = insertarDatos.insertarLibroEnDOM("Autor1", "TItulo1", "Genero1", "Precio1", "Fecha1", "Descripcion1", f, doc);
+            //Lectura del DOM con todas las modificaciones realizadas
             leerAccesoDom.mostrarLibros(doc);
+            
+            //Guardar en el Nuevo archivo
+            guardarDomNuevoArchivo.guardarDOMcomoArchivo("PruebaLuisY_Alejandro.xml", doc);
+            //Mostrar el nuevo archivo
         } 
         catch (UnsupportedEncodingException e) {
             System.out.println(e);
